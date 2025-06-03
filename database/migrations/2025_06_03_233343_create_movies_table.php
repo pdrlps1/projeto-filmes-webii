1c0b0c58390b0b3db089a0b5b8c8829ce2934379
pdrlps1 <pedro@DESKTOP-JRJGPC2.>
2025-06-03 20:39:39 -0300

    filmes table migration

---
+++ database/migrations/2025_06_03_233343_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('year');
+            $table->decimal('rating', 3, 1)->nullable();
+            $table->text('review')->nullable();
+            $table->boolean('watched')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
